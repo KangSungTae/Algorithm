60cf0f16fecd1dd707b08c4d4f76869de1475504
DESKTOP-TNBF7J3\kangs <kangs20511@gmail.com>
2022-05-26 14:13:17 +0900

    프로그래머스 괄호 회전하기 레벨2 문제 : stack 을 사용하여 짝이 맞을 경우 stack의 값을 pop 한다. 최종 스택 size가 0일 경우 true 리턴.

---
+++ Algorithm/src/bracketAlgo.java
@@ -0,0 +1,81 @@
+import java.util.Stack;
+
+public class bracketAlgo {
+
+	public static void main(String[] args) {
+		
+		/* input 값 a 가 좌측으로 length 만큼 이동할때 완벽한 문자열이 되는 개수를 구하라.*/
+		/* java.util 의 stack 은 push , pop(값 호출 후 제거) , peek(값 확인) 3가지로 나누어져 있음. */
+		 
+		String b = "{{{{{{{";
+//		checkCorrect(b);
+//		solution(a);
+		System.out.println(""+solution(b));
+	}
+	
+	/* 좌측으로 i칸씩 이동하며 완벽한 문자열을 찾는 함수를 호출 */
+    public static int solution(String s) {
+        int answer = 0;
+        
+        for(int i = 0 ; i < s.length() ; i ++) {
+        	/* 좌측으로 i칸씩 */
+        	String k = s.substring(i, s.length()) + s.substring(0,i);
+            if(checkCorrect(k)) {
+            	answer += 1;
+            }
+        }
+
+        return answer;
+    }
+    
+    /* 완벽한 문자열 check 함수. */
+    public static boolean checkCorrect(String s) {
+		/* stack 을 활용하여 구현이 가능 */
+    	
+    	Stack<Object> stack = new Stack<Object>();
+    	
+    	for(int i = 0 ; i < s.length() ; i ++) {
+    		switch (s.charAt(i)) {
+			case '}' : {
+				try{
+					if(stack.peek().equals('{')) {
+						stack.pop();
+						break;
+					}else {
+						return false;
+					}
+				}catch(Exception e ) {
+					return false;
+				} 
+			}case ']' : {
+				try {
+					if(stack.peek().equals('[')) {
+						stack.pop();
+						break;
+					}else {
+						return false;
+					}
+				}catch (Exception e) {
+					return false;
+				} 
+			}case ')' : {
+				try {
+					if(stack.peek().equals('(')) {
+						stack.pop();
+						break;
+					}else {
+						return false;
+					}
+				}catch(Exception e) {
+					return false;
+				}
+			}
+			default:
+				stack.add(s.charAt(i));
+			}
+    	}
+    	
+    	return stack.size() == 0 ? true : false;
+    }
+    
+}
